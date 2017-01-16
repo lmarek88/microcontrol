9cc1089cd0e134b1311d350221ddd667476a7e1c
lmarek88 <lukash1988@gmail.com>
2017-01-16 23:02:54 +0100

    add logo

--- src/main/webapp/script/app.js
+++ src/main/webapp/script/app.js
@@ -2,16 +2,34 @@
 (function () {
 
     var bindNavigation = function () {
+        $('.navbar-brand').click(function(){
+            setActiveContent('main');
+        });
+        
         $('.navbar-nav li').on("click", function (e) {
-            $('.navbar-nav li.active').removeClass('active');
+//            $('.navbar-nav li.active').removeClass('active');
             var element = $(e.currentTarget);
-            element.addClass('active');
+            
+//            element.addClass('active');
             var id = element.data("id");
-            $('.screen').hide();
-            $('#' + id).show();
+            setActiveContent(id);
+//            $('.screen').hide();
+//            $('#' + id).show();
         });
         $('.navbar-nav li[data-id="gallery"]').trigger('click');
     }
+    
+    var setActiveContent = function(id){
+        $('.navbar-nav li.active').removeClass('active');
+        var element = $('#' + id);
+        $('.navbar-nav li[data-id="' + id + '"').addClass('active');
+        $('.screen').hide();
+        element.show()
+    }
+    
+    var mainColor = $('.navbar').css('background-color');
+    $('#contact').css('color', mainColor + "!important");
+    
 
     
 
